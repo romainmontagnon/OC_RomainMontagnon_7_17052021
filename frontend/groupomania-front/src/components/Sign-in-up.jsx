db4942d7ee8d3c0df453e1b12cd675fd7e779581
romainmontagnon <montagnon.romain@gmail.com>
2021-07-12 21:48:36 +0200

    fin frontend connexion/inscription, reste lien entre front et back

--- frontend/groupomania-front/src/components/Sign-in-up.jsx
+++ frontend/groupomania-front/src/components/Sign-in-up.jsx
@@ -6,10 +6,10 @@ const SignInUpButton = () => {
     const [showSignIn, setshowSignIn] = useState(false);
     const [showSignUp, setshowSignUP] = useState(true);
     return (
-        <div>
-            <div className='flex flex-row'>
+        <div className='h-80 flex flex-row bg-white bg-opacity-40 pr-10 rounded-3xl'>
+            <div className='flex flex-col justify-around'>
                 <button
-                    className='mr-1 rounded-2xl px-4 ring-2 ring-midnight-400 text-center text-midnight-500 bg-midnight-200 font-semibold hover:bg-midnight-400 hover:text-midnight-100 uppercase'
+                    className='transform -rotate-90 mr-1 rounded-2xl px-4 ring-2 ring-midnight-400 text-center text-midnight-500 bg-midnight-200 font-semibold hover:bg-midnight-400 hover:text-midnight-100 uppercase'
                     onClick={() => {
                         setshowSignUP(true)
                         setshowSignIn(false)
@@ -17,7 +17,7 @@ const SignInUpButton = () => {
                     Inscription
                 </button>
                 <button
-                    className='mr-1 rounded-2xl px-4 ring-2 ring-midnight-400 text-center text-midnight-500 bg-midnight-200 font-semibold hover:bg-midnight-400 hover:text-midnight-100 uppercase'
+                    className='transform -rotate-90 mr-1 rounded-2xl px-4 ring-2 ring-midnight-400 text-center text-midnight-500 bg-midnight-200 font-semibold hover:bg-midnight-400 hover:text-midnight-100 uppercase'
                     onClick={() => {
                         setshowSignUP(false)
                         setshowSignIn(true)
@@ -25,7 +25,7 @@ const SignInUpButton = () => {
                     Login
                 </button>
             </div>
-            <div>
+            <div className='my-auto'>
                 {showSignUp && <SignUpForm />}
                 {showSignIn && <SignInForm />}
             </div>
--- frontend/groupomania-front/src/components/sign-in-up/SignUpForm.jsx
+++ frontend/groupomania-front/src/components/sign-in-up/SignUpForm.jsx
@@ -1,38 +1,41 @@
 import React from 'react';
 const SignUpForm = () => {
     return (
-        <div className='w-80 h-96 border-solid border- text-black'>
-            <div className='flex flex-col'>
-                <h2 className='uppercase font-semi-bold h-2'>Inscription</h2>
-                <form>
-                    <input
-                        type="text"
-                        id="firstname-signup"
-                        placeholder="Nom"
-                        name="firstname"
-                        className='bg-midnight-100 ring-2 ring-midnight-400 hover:bg-midnight-50 focus:outline-none focus:ring-2 focus:bg-midnight-50 focus:ring-opacity-50 rounded-2xl w-60 text-center' />
-                    <input
-                        type="text"
-                        id="lastname-signup"
-                        placeholder="Prénom"
-                        name="lastname"
-                        className='bg-midnight-100 ring-2 ring-midnight-400 hover:bg-midnight-50 focus:outline-none focus:ring-2 focus:bg-midnight-50 focus:ring-opacity-50 rounded-2xl w-60 text-center' />
-                    <input
-                        type="email"
-                        id="email-signup"
-                        placeholder="john.doe@groupomania.eu"
-                        name="email"
-                        className='bg-midnight-100 ring-2 ring-midnight-400 hover:bg-midnight-50 focus:outline-none focus:ring-2 focus:bg-midnight-50 focus:ring-opacity-50 rounded-2xl w-60 text-center' />
-                    <input
-                        type="password"
-                        id="password-signup"
-                        placeholder="Mot de passe"
-                        name="password"
-                        className='bg-midnight-100 ring-2 ring-midnight-400 hover:bg-midnight-50 focus:outline-none focus:ring-2 focus:bg-midnight-50 focus:ring-opacity-50 rounded-2xl w-60 text-center' />
+        <div className='flex flex-col justify-around'>
+            {/* <div className=''> */}
+            <h2 className='uppercase font-semi-bold text-2xl text-center text-midnight-500 mb-10'>Inscription</h2>
+            <form className='flex flex-col justify-around my-1'>
+                <input
+                    type="text"
+                    id="firstname-signup"
+                    placeholder="Nom"
+                    name="firstname"
+                    className='bg-midnight-100 ring-2 ring-midnight-400 hover:bg-midnight-50 focus:outline-none focus:ring-2 focus:bg-midnight-50 focus:ring-opacity-50 rounded-2xl w-60 text-center my-2' />
+                <input
+                    type="text"
+                    id="lastname-signup"
+                    placeholder="Prénom"
+                    name="lastname"
+                    className='bg-midnight-100 ring-2 ring-midnight-400 hover:bg-midnight-50 focus:outline-none focus:ring-2 focus:bg-midnight-50 focus:ring-opacity-50 rounded-2xl w-60 text-center my-2' />
+                <input
+                    type="email"
+                    id="email-signup"
+                    placeholder="john.doe@groupomania.eu"
+                    name="email"
+                    className='bg-midnight-100 ring-2 ring-midnight-400 hover:bg-midnight-50 focus:outline-none focus:ring-2 focus:bg-midnight-50 focus:ring-opacity-50 rounded-2xl w-60 text-center my-2' />
+                <input
+                    type="password"
+                    id="password-signup"
+                    placeholder="Mot de passe"
+                    name="password"
+                    className='bg-midnight-100 ring-2 ring-midnight-400 hover:bg-midnight-50 focus:outline-none focus:ring-2 focus:bg-midnight-50 focus:ring-opacity-50 rounded-2xl w-60 text-center my-2' />
 
-                    <input type="submit" value="Envoyer" />
-                </form>
-            </div>
+                <input
+                    type="submit"
+                    value="Envoyer"
+                    className='rounded-2xl px-4 ring-2 ring-midnight-400 text-center text-midnight-500 bg-midnight-200 font-semibold hover:bg-midnight-400 hover:text-midnight-100 uppercase my-2' />
+            </form>
+            {/* </div> */}
         </div>
     )
 };
