2c99f1ceca1b36d59c780088538e1b3c4dcf0f84
romainmontagnon <montagnon.romain@gmail.com>
2021-07-26 10:59:36 +0200

    transmission info de feeds a feed

--- frontend/groupomania-front/src/components/Feeds.jsx
+++ frontend/groupomania-front/src/components/Feeds.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Comment from './Comment';
 import Feed from './Feed';
+import HelloWorld from './HelloWorld'
 import ShowComments from './ShowComments';
 import { storeToSessionStorage, loadFromSessionStorage } from '../js/sesssion';
 import { routes } from '../js/routes';
@@ -10,7 +11,7 @@ import { faHandPointLeft } from '@fortawesome/free-solid-svg-icons';
 class Feeds extends React.Component {
     constructor(props) {
         super(props);
-        this.isLoggedIn = this.props.isLoggedIn
+        // this.isLoggedIn = this.props.isLoggedIn
         this.allFeeds = this.props.allFeeds.allFeeds
     }
 
@@ -19,36 +20,36 @@ class Feeds extends React.Component {
             console.log('hello component update')
             // console.table(this.props.allFeeds.allFeeds)
             var feed = this.props.allFeeds.allFeeds;
-            for (let i = 0; i < feed.length; i++) {
-                const element = feed[i];
-                console.log(element)
-            }
+            // for (let i = 0; i < feed.length; i++) {
+            //     const element = feed[i];
+            //     console.log(element)
+            // }
+            return (
+                <ul>
+                    {feed.map((oneFeed, index) =>
+                        // {console.log(`${index+100}`)
+                        // console.log(oneFeed)}
+                        <li>
+                            <Feed key={`${index + 100}`} oneFeed={oneFeed} />
+                        </li>
+                    )}
+                </ul>
+            )
         } else {
-            return null
+            return <HelloWorld />
         };
     }
 
     render() {
-        const styleBorderRed = { border: 'solid 1px red' }
-        // const test = ['A', 'B', 'C']
         return (
-
-            // <ul>
-            //     {this.test.map(item => (
-            //         <li key={item}>{item}</li>
-            //     ))}
-            // </ul>
             <div className='w-3/4 mx-auto my-10 bg-white bg-opacity-40 px-4 py-4 rounded-3xl'>
                 <div
                     className='flex flex-col justify-around items-center border-solid border-b-2 border-mandy-500 border-opacity-80'>
-                    <div className='w-5/6 flex flex-col' style={styleBorderRed}>
-                        one feed
-                        <ul>
-                            <Feed />
-                        </ul>
-                        <div className='my-1 divide-y-4 divide-mandy-700 divide-solid'>
+                    <div className='w-5/6 flex flex-col' style={{ border: 'solid 1px red' }}>
+                        {this.componentDidUpdate()}
+                        {/* <div className='my-1 divide-y-4 divide-mandy-700 divide-solid'>
                             <Comment />
-                        </div>
+                        </div> */}
                     </div>
                 </div>
             </div>
